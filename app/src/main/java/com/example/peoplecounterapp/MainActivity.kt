9c96256b3e52e8dca22503cccc33b5411ab8e83b
agrwl-prankit <agrwnlprankit77@gmail.com>
2022-10-05 16:32:01 +0530

    Add textView and Buttons

--- app/src/main/java/com/example/peoplecounterapp/MainActivity.kt
+++ app/src/main/java/com/example/peoplecounterapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.peoplecounterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+
+        var timeClicked = 0
+
+        clickMeButton.setOnClickListener {
+            timeClicked++
+            textView.text = "You clicked the button $timeClicked times !!"
+        }
+
+        resetButton.setOnClickListener {
+            timeClicked = 0
+            textView.text = "You clicked the button $timeClicked times !!"
+        }
     }
 }
